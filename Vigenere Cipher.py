b33b576bbc1cd15a888d02fa63bc1940b6b226a0
Tsavo-ka <80832277+Tsavo-ka@users.noreply.github.com>
2022-10-13 11:46:57 +0300

    Create Vigenere Cipher.py

---
+++ Vigenere Cipher.py
@@ -0,0 +1,136 @@
+import string
+letters = [x for x in string.ascii_lowercase]
+
+my_keys = {}
+counter = 0
+for l in letters:
+    my_keys[l] = counter
+    counter += 1
+
+def key_build(text, key):
+    # This function will stretch or compress the user's key to match the length of the text, accounting for special characters
+    new = ""
+    ind = 0
+    
+    while len(new) < len(text):
+        
+        for c in text:
+            
+            if not c.isalpha():
+                new += c
+            else:
+                new += key[ind].lower()
+                if ind == len(key)-1:
+                    ind = 0
+                else:
+                    ind += 1
+            
+    return new
+
+def encrypt(text, key):
+    # This function takes in the user's text and DECRYPTS it against the user's key, returning cypher text
+    cypher = []
+    l_text = text.lower()
+    
+    key = key_build(text, key)
+    
+
+    for i, c in enumerate(l_text):
+        if not c.isalpha():
+            cypher += c
+        elif ord(c) + my_keys[key[i]] > 122:
+            cypher += chr(abs(ord(c) + my_keys[key[i]] - 123) + 97)
+        else:
+            cypher += chr(ord(c) + my_keys[key[i]])
+        
+    for i, c in enumerate(text):
+        if c.isupper():
+            cypher[i] = cypher[i].upper()
+    
+    return "".join(cypher)
+
+def decrypt(text, key):
+    
+    plain = []
+    l_text = text.lower()
+    
+    key = key_build(text, key)
+    
+
+    for i, c in enumerate(l_text):
+        if not c.isalpha():
+            plain += c
+        elif ord(c) - my_keys[key[i]] < 97:
+            plain += chr(abs(abs(my_keys[c] - my_keys[key[i]]) - 123))
+        else:
+            plain += chr(ord(c) - my_keys[key[i]])
+        
+    for i, c in enumerate(text):
+        if c.isupper():
+            plain[i] = plain[i].upper()
+    
+    return "".join(plain)
+
+print(""",   .     .                     .         ,  ,       .   .   p     
+| . |     |                     |         | /      o |   |         
+| ) ) ,-. | ,-. ,-. ;-.-. ,-.   |-  ,-.   |<   ,-. . |-  |-.   ,-. 
+|/|/  |-' | |   | | | | | |-'   |   | |   | \\  |-' | |   | |   `-. 
+' '   `-' ' `-' `-' ' ' ' `-'   `-' `-'   '  ` `-' ' `-' ' '   `-' 
+                                                                   """)
+
+print("""██╗   ██╗██╗ ██████╗ ███████╗███╗   ██╗███████╗██████╗ ███████╗     ██████╗██╗██████╗ ██╗  ██╗███████╗██████╗ 
+██║   ██║██║██╔════╝ ██╔════╝████╗  ██║██╔════╝██╔══██╗██╔════╝    ██╔════╝██║██╔══██╗██║  ██║██╔════╝██╔══██╗
+██║   ██║██║██║  ███╗█████╗  ██╔██╗ ██║█████╗  ██████╔╝█████╗      ██║     ██║██████╔╝███████║█████╗  ██████╔╝
+╚██╗ ██╔╝██║██║   ██║██╔══╝  ██║╚██╗██║██╔══╝  ██╔══██╗██╔══╝      ██║     ██║██╔═══╝ ██╔══██║██╔══╝  ██╔══██╗
+ ╚████╔╝ ██║╚██████╔╝███████╗██║ ╚████║███████╗██║  ██║███████╗    ╚██████╗██║██║     ██║  ██║███████╗██║  ██║
+  ╚═══╝  ╚═╝ ╚═════╝ ╚══════╝╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝╚══════╝     ╚═════╝╚═╝╚═╝     ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
+                                                                                                              """)
+print(""" ██████╗ ███████╗███╗   ██╗███████╗██████╗  █████╗ ████████╗ ██████╗ ██████╗ 
+██╔════╝ ██╔════╝████╗  ██║██╔════╝██╔══██╗██╔══██╗╚══██╔══╝██╔═══██╗██╔══██╗
+██║  ███╗█████╗  ██╔██╗ ██║█████╗  ██████╔╝███████║   ██║   ██║   ██║██████╔╝
+██║   ██║██╔══╝  ██║╚██╗██║██╔══╝  ██╔══██╗██╔══██║   ██║   ██║   ██║██╔══██╗
+╚██████╔╝███████╗██║ ╚████║███████╗██║  ██║██║  ██║   ██║   ╚██████╔╝██║  ██║
+ ╚═════╝ ╚══════╝╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝
+                                                                             """)
+
+
+run = True
+
+while run:
+
+    #Choose encryption or decryption function
+    while True:
+        to_do = input("Hello, user. Will you be encrypting or decrypting a message? (enter 'e' or 'd'): ").lower()
+        if to_do.startswith('e') or to_do.startswith('d'):
+            break
+        else:
+            print("\nplease enter a valid answer\n")
+
+    text = ""
+    key = ""
+
+    # Take a text to encrypt/decrypt
+    while not text:
+        text = input("\nPlease input a message to encrypt/decrypt: ")
+        if not text:
+            print("\nPlease enter a message of at least 1 character")
+
+    # Take a key for encryption/decryption
+    while not key or not key.isalpha():
+        key = input("\nPlease enter an encryption key: ")
+        if not key:
+            print("\nPlease enter a key of at least 1 character (longer key recommended")
+        elif not key.isalpha():
+            print("\nYour key cannot contain numbers or special characters")
+
+    # Run encryption/decryption depending on user's initial choice
+    if to_do.startswith('e'):
+        new_text = encrypt(text, key)
+    else:
+        new_text = decrypt(text, key)
+
+    
+    if to_do.startswith('e'):
+        print(f"\nYour encrypted text:\n\n\t{new_text}\n")
+    else:
+        print(f"\nYour decrypted text:\n\n\t{new_text}\n")
