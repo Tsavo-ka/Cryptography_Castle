fcd414f333aafd356552d9c9f80e6cf21e5e47d0
Tsavo-ka <80832277+Tsavo-ka@users.noreply.github.com>
2023-09-22 12:19:09 +0200

    Update Vigenere Cipher.py
    Improvements made to code

--- Vigenere Cipher.py
+++ Vigenere Cipher.py
@@ -1,141 +1,66 @@
 import string
-letters = [x for x in string.ascii_lowercase]
 
-my_keys = {}
-counter = 0
-for l in letters:
-    my_keys[l] = counter
-    counter += 1
+ALPHABET = string.ascii_lowercase
 
-def key_build(text, key):
-    '''
-    This function will stretch or compress the user's key to match the length of the text, accounting for special characters
-    '''
-    new = ""
-    ind = 0
-    
-    while len(new) < len(text):
-        
-        for c in text:
-            
-            if not c.isalpha():
-                new += c
-            else:
-                new += key[ind].lower()
-                if ind == len(key)-1:
-                    ind = 0
-                else:
-                    ind += 1
-            
-    return new
+def build_key(text, key):
+    new_key = ''
+    key_index = 0
 
-def encrypt(text, key):
-    '''
-    This function takes in the user's text and ENCRYPTS it against the user's key, returning cypher text
-    '''
-    cypher = []
-    l_text = text.lower()
-    
-    key = key_build(text, key)
-    
-
-    for i, c in enumerate(l_text):
-        if not c.isalpha():
-            cypher += c
-        elif ord(c) + my_keys[key[i]] > 122:
-            cypher += chr(abs(ord(c) + my_keys[key[i]] - 123) + 97)
+    for char in text:
+        if char.isalpha():
+            new_key += key[key_index].lower()
+            key_index = (key_index + 1) % len(key)
         else:
-            cypher += chr(ord(c) + my_keys[key[i]])
-        
-    for i, c in enumerate(text):
-        if c.isupper():
-            cypher[i] = cypher[i].upper()
-    
-    return "".join(cypher)
+            new_key += char
 
-def decrypt(text, key):
-    '''
-    This function takes in the user's text and DECRYPTS it against the user's key, returning plain text
-    '''
-    plain = []
-    l_text = text.lower()
-    
-    key = key_build(text, key)
-    
+    return new_key
 
-    for i, c in enumerate(l_text):
-        if not c.isalpha():
-            plain += c
-        elif ord(c) - my_keys[key[i]] < 97:
-            plain += chr(abs(abs(my_keys[c] - my_keys[key[i]]) - 123))
+def vigenere_cipher(text, key, decrypt=False):
+    result = []
+    key = build_key(text, key)
+    alphabet_len = len(ALPHABET)
+
+    for i, char in enumerate(text):
+        if char.isalpha():
+            shift = ALPHABET.index(key[i]) if not decrypt else -ALPHABET.index(key[i])
+            new_char = ALPHABET[(ALPHABET.index(char.lower()) + shift) % alphabet_len]
+
+            if char.isupper():
+                new_char = new_char.upper()
+
+            result.append(new_char)
         else:
-            plain += chr(ord(c) - my_keys[key[i]])
-        
-    for i, c in enumerate(text):
-        if c.isupper():
-            plain[i] = plain[i].upper()
-    
-    return "".join(plain)
+            result.append(char)
 
-print(""",   .     .                     .         ,  ,       .   .   p     
-| . |     |                     |         | /      o |   |         
-| ) ) ,-. | ,-. ,-. ;-.-. ,-.   |-  ,-.   |<   ,-. . |-  |-.   ,-. 
-|/|/  |-' | |   | | | | | |-'   |   | |   | \\  |-' | |   | |   `-. 
-' '   `-' ' `-' `-' ' ' ' `-'   `-' `-'   '  ` `-' ' `-' ' '   `-' 
-                                                                   """)
+    return ''.join(result)
 
-print("""██╗   ██╗██╗ ██████╗ ███████╗███╗   ██╗███████╗██████╗ ███████╗     ██████╗██╗██████╗ ██╗  ██╗███████╗██████╗ 
-██║   ██║██║██╔════╝ ██╔════╝████╗  ██║██╔════╝██╔══██╗██╔════╝    ██╔════╝██║██╔══██╗██║  ██║██╔════╝██╔══██╗
-██║   ██║██║██║  ███╗█████╗  ██╔██╗ ██║█████╗  ██████╔╝█████╗      ██║     ██║██████╔╝███████║█████╗  ██████╔╝
-╚██╗ ██╔╝██║██║   ██║██╔══╝  ██║╚██╗██║██╔══╝  ██╔══██╗██╔══╝      ██║     ██║██╔═══╝ ██╔══██║██╔══╝  ██╔══██╗
- ╚████╔╝ ██║╚██████╔╝███████╗██║ ╚████║███████╗██║  ██║███████╗    ╚██████╗██║██║     ██║  ██║███████╗██║  ██║
-  ╚═══╝  ╚═╝ ╚═════╝ ╚══════╝╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝╚══════╝     ╚═════╝╚═╝╚═╝     ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
-                                                                                                              """)
-print(""" ██████╗ ███████╗███╗   ██╗███████╗██████╗  █████╗ ████████╗ ██████╗ ██████╗ 
-██╔════╝ ██╔════╝████╗  ██║██╔════╝██╔══██╗██╔══██╗╚══██╔══╝██╔═══██╗██╔══██╗
-██║  ███╗█████╗  ██╔██╗ ██║█████╗  ██████╔╝███████║   ██║   ██║   ██║██████╔╝
-██║   ██║██╔══╝  ██║╚██╗██║██╔══╝  ██╔══██╗██╔══██║   ██║   ██║   ██║██╔══██╗
-╚██████╔╝███████╗██║ ╚████║███████╗██║  ██║██║  ██║   ██║   ╚██████╔╝██║  ██║
- ╚═════╝ ╚══════╝╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝
-                                                                             """)
-
-
-run = True
-
-while run:
-
-    #Choose encryption or decryption function
+def main():
     while True:
         operation = input("Hello, user. Will you be encrypting or decrypting a message? (enter 'e' or 'd'): ").lower()
-        if operation.startswith('e') or operation.startswith('d'):
-            break
+
+        if operation.startswith('e'):
+            decrypt = False
+        elif operation.startswith('d'):
+            decrypt = True
         else:
-            print("\nplease enter a valid answer\n")
+            print("\nPlease enter 'e' for encryption or 'd' for decryption.\n")
+            continue
 
-    # Take a text to encrypt/decrypt
-    while True:
         text = input("Enter the text: ")
-        if text:
-            break
-        print('Enter a text of at least 1 alphanumeric character')
-    
+        if not text:
+            print('Enter a text of at least 1 alphanumeric character.\n')
+            continue
 
-    # Take a key for encryption/decryption
-    while not key or not key.isalpha():
-        key = input("\nPlease enter an encryption key: ")
+        key = input("\nEnter an encryption key: ")
         if not key:
-            print("\nPlease enter a key of at least 1 character (longer key recommended")
+            print("\nPlease enter a key of at least 1 character (longer key recommended).\n")
+            continue
         elif not key.isalpha():
-            print("\nYour key cannot contain numbers or special characters")
+            print("\nYour key cannot contain numbers or special characters.\n")
+            continue
 
-    # Run encryption/decryption depending on user's initial choice
-    if operation.startswith('e'):
-        new_text = encrypt(text, key)
-    else:
-        new_text = decrypt(text, key)
+        result = vigenere_cipher(text, key, decrypt)
+        print('\nResult:', result, '\n')
 
-    
-    if operation.startswith('e'):
-        print(f"\nYour encrypted text:\n\n\t{new_text}\n")
-    else:
-        print(f"\nYour decrypted text:\n\n\t{new_text}\n")
+if __name__ == "__main__":
+    main()
