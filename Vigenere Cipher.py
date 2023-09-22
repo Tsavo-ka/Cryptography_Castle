2c018ef22558d54e0d384c9946c875179859f76f
Tsavo-ka <80832277+Tsavo-ka@users.noreply.github.com>
2023-09-22 12:39:59 +0200

    Update Vigenere Cipher.py

--- Vigenere Cipher.py
+++ Vigenere Cipher.py
@@ -3,9 +3,15 @@ import string
 ALPHABET = string.ascii_lowercase
 
 def build_key(text, key):
+    '''
+    Stretch a key to match the length of the text
+    '''
+
+    # Starting blank key and beginning index
     new_key = ''
     key_index = 0
 
+    # extend key to match length of text, accounting for spaces and puncuation
     for char in text:
         if char.isalpha():
             new_key += key[key_index].lower()
@@ -16,19 +22,26 @@ def build_key(text, key):
     return new_key
 
 def vigenere_cipher(text, key, decrypt=False):
+    '''
+    Encrypt or decrypt (depending on the decrypt flag) text against key.
+    '''
+    # parameters - empty new text, extended key, and value of 26
     result = []
     key = build_key(text, key)
-    alphabet_len = len(ALPHABET)
+    alphabet_len = 26
 
+    # Calculates and applies letter shift
     for i, char in enumerate(text):
         if char.isalpha():
             shift = ALPHABET.index(key[i]) if not decrypt else -ALPHABET.index(key[i])
             new_char = ALPHABET[(ALPHABET.index(char.lower()) + shift) % alphabet_len]
 
+            # Matches uppercase letters in new text
             if char.isupper():
                 new_char = new_char.upper()
 
             result.append(new_char)
+        # Append spaces or puncuation unchanged
         else:
             result.append(char)
 
