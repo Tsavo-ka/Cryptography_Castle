af1690a858efedad566025b61065b9d4017fb236
Tsavo-ka <80832277+Tsavo-ka@users.noreply.github.com>
2023-10-19 09:53:02 +0200

    Update vigenere.py

--- vigenere.py
+++ vigenere.py
@@ -1,53 +1,70 @@
 import string
 
+# Define the English alphabet in lowercase
 ALPHABET = string.ascii_lowercase
 
+# Function to build a key of the same length as the input text
 def build_key(text, key):
-    '''
-    Stretch a key to match the length of the text
-    '''
-    # Starting blank key and beginning index
     new_key = ''
     key_index = 0
 
-    # extend key to match length of text, accounting for spaces and puncuation
     for char in text:
         if char.isalpha():
+            # Replicate the key character to match the length of the text
             new_key += key[key_index].lower()
             key_index = (key_index + 1) % len(key)
         else:
+            # If the character is not alphabetic, keep it as is in the key
             new_key += char
 
     return new_key
 
+# Function to perform Vigenère encryption or decryption
 def vigenere_cipher(text, key, decrypt=False):
-    '''
-    Encrypt or decrypt (depending on the decrypt flag) text against key.
-    '''
-    # parameters - empty new text, extended key, and value of 26
     result = []
-    key = build_key(text, key)
-    alphabet_len = 26
+    key = build_key(text, key)  # Build the key of the same length as the text
+    alphabet_len = len(ALPHABET)
 
-    # Calculates and applies letter shift
     for i, char in enumerate(text):
         if char.isalpha():
+            # Calculate the shift based on the key character
             shift = ALPHABET.index(key[i]) if not decrypt else -ALPHABET.index(key[i])
+            # Apply the shift to the character
             new_char = ALPHABET[(ALPHABET.index(char.lower()) + shift) % alphabet_len]
 
-            # Matches uppercase letters in new text
             if char.isupper():
                 new_char = new_char.upper()
 
             result.append(new_char)
-        # Append spaces or puncuation unchanged
         else:
             result.append(char)
 
     return ''.join(result)
-    
+
+# Function to ask the user if they want to repeat the process
+def replay():
+    while True:
+        re = input('Go again? (\'y\' or \'n\'): ')
+        if re.startswith('y') or re.startswith('n'):
+            return re.startswith('y')
+        print('Invalid input')
+
+# Main function to run the Vigenère cipher program
 def main():
-    # User's Choice (Encrypt or Decrypt)
+    print('''Welcome to Vault!
+    
+    This program is designed to teach encryption concepts as well as Python code.
+    Here is a brief walkthrough of how Vigenère encryption works and how it is implemented in this code:
+    
+    1. The user must first input a text string
+    2. The user must then input a text key
+    3. This program will then compress or extend the key using replication until it matches the length of the text.
+    4. The user will specify whether to use the encryption or decryption algorithm
+    5. The program will then apply a shift to each letter of the text according to the corresponding letter of the key and the user's choice of encryption or decryption
+    6. This program returns the ciphertext or plain text to the user
+    
+    I hope this made sense. Enjoy!\n''')
+
     while True:
         operation = input("Hello, user. Will you be encrypting or decrypting a message? (enter 'e' or 'd'): ").lower()
 
@@ -62,8 +79,8 @@ def main():
         # Step 5: Input Text
         while True:
             text = input("Enter the text: ")
-            if not any([x.isalpha() for x in text]):
-                print('Enter a text with at least 1 alphanumeric character.\n')
+            if not text:
+                print('Enter a text of at least 1 alphanumeric character.\n')
             else:
                 break
 
@@ -81,5 +98,10 @@ def main():
         result = vigenere_cipher(text, key, decrypt)
         print('\nResult:', result, '\n')
 
+        repeat = replay()
+
+        if not repeat:
+            break
+
 if __name__ == "__main__":
-    main()
+    main()  # Run the main program
